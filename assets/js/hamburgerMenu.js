255974098745fe779c17d640e285116bb274128f
Bekter <besedov347@gmail.com>
2023-08-29 14:12:17 +0300

    contact us page added

--- assets/js/hamburgerMenu.js
+++ assets/js/hamburgerMenu.js
@@ -5,9 +5,13 @@ const nav_list = document.querySelector(".nav-list");
 hamburgerBtn.addEventListener("click", () => {
   absolute.style.display = "block";
   nav_list.style.display = "flex";
+  nav_list.style.right = "17px";
 });
 
 cancel.addEventListener("click", () => {
   absolute.style.display = "none";
-  nav_list.style.display = "none";
+  nav_list.style.right = "-400px";
+});
+window.addEventListener("resize", function (event) {
+  document.body.clientWidth > 742 && (absolute.style.display = "none");
 });
--- assets/js/join.js
+++ assets/js/join.js
@@ -1,10 +1,13 @@
 const header_icon = document.querySelector(".header-icon");
 const join = document.querySelector(".join");
 const canc = document.querySelector(".canc");
+const dark = document.querySelector(".dark");
 header_icon.addEventListener("click", () => {
   join.style.display = "block";
+  dark.style.display = "block";
 });
 canc.addEventListener("click", () => {
   join.style.display = "none";
+  dark.style.display = "none";
 });
 console.log(header_icon);
